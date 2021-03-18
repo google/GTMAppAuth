ed775bcbba7a0aef028eb4eeff3a47d997b1fcd4
Peter Andrews <petea@google.com>
2021-03-18 16:08:39 -0700

    Set min platform version to 9.0 for iOS.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     name: "GTMAppAuth",
     platforms: [
         .macOS(.v10_11),
-        .iOS(.v8),
+        .iOS(.v9),
         .tvOS(.v9),
         .watchOS(.v6)
     ],
