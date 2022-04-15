a39682e2e0b24e06864e91beb2c3546ea4970641
pinlu <pl1223@nyu.edu>
2022-04-14 17:41:55 -0700

    Fix spm-build-test

--- Package.swift
+++ Package.swift
@@ -56,7 +56,7 @@ let package = Package(
                 .linkedFramework("Security"),
 	    ]
         ),
-        .target(
+        .testTarget(
  	    name: "GTMAppAuth-UnitTests",
 	    dependencies: [
 	    	"GTMAppAuth",
