b2dda8c0a69e0bfb3d474ab786d0058b58ebdf53
Matthew Mathias <mdmathias@google.com>
2022-05-26 17:06:23 -0700

    Update GTMKeychain to use Swift

--- GTMAppAuthSwift/Tests/GTMKeychainTests.swift
+++ GTMAppAuthSwift/Tests/GTMKeychainTests.swift
@@ -15,11 +15,124 @@
  */
 
 import XCTest
-import GTMAppAuthSwift
+@testable import GTMAppAuthSwift
 
 class GTMKeychainTests: XCTestCase {
-  func testKeychainInstance() {
-    let gtmKeychain = GTMKeychain()
-    XCTAssertNotNil(gtmKeychain)
+  private let keychainHelper = KeychainHelperFake()
+  private let testPassword = "foo"
+  private let testKeychainItemName = "testName"
+  private lazy var keychain: GTMKeychain = {
+    return GTMKeychain(keychainHelper: keychainHelper)
+  }()
+
+  override func setUp() {
+    super.setUp()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+    keychainHelper.passwordStore.removeAll()
+  }
+
+  func testStringPassword() throws {
+    try keychain.save(password: testPassword, forName: testKeychainItemName)
+    let expectedPassword = try keychain.password(forName: testKeychainItemName)
+    XCTAssertEqual(expectedPassword, testPassword)
+    try keychain.removePasswordFromKeychain(forName: testKeychainItemName)
+  }
+
+  func testDataPassword() throws {
+    guard let passwordData = testPassword.data(using: .utf8) else {
+      return XCTFail("Could not convert `testPassword` into `Data`.")
+    }
+    try keychain.save(passwordData: passwordData, forName: testKeychainItemName)
+    let expectedPasswordData = try keychain.passwordData(forName: testKeychainItemName)
+    XCTAssertEqual(expectedPasswordData, testPassword.data(using: .utf8)!)
+    try keychain.removePasswordFromKeychain(forName: testKeychainItemName)
+  }
+
+  func testSetPasswordNoService() {
+    XCTAssertThrowsError(try keychain.save(password: testPassword, forName: "")) { thrownError in
+      XCTAssertEqual(thrownError as? KeychainWrapper.Error, .noService)
+    }
+  }
+
+  func testReadPasswordNoService() throws {
+    try keychain.save(password: testPassword, forName: testKeychainItemName)
+
+    XCTAssertThrowsError(try keychain.password(forName: "")) { thrownError in
+      XCTAssertEqual(thrownError as? KeychainWrapper.Error, .noService)
+    }
+  }
+
+  func testRemovePasswordNoService() throws {
+    try keychain.save(password: testPassword, forName: testKeychainItemName)
+
+    XCTAssertThrowsError(try keychain.removePasswordFromKeychain(forName: "")) { thrownError in
+      XCTAssertEqual(thrownError as? KeychainWrapper.Error, .noService)
+    }
+  }
+
+  func testFailedToDeletePasswordError() {
+    XCTAssertThrowsError(try keychain.removePasswordFromKeychain(
+      forName: testKeychainItemName
+    )) { thrownError in
+      XCTAssertEqual(thrownError as? KeychainWrapper.Error, .failedToDeletePassword)
+    }
+  }
+
+  func testPasswordNotFoundError() {
+    XCTAssertThrowsError(try keychain.password(forName: testKeychainItemName)) { thrownError in
+      XCTAssertEqual(thrownError as? KeychainWrapper.Error, .passwordNotFound)
+    }
+  }
+}
+
+fileprivate class KeychainHelperFake: KeychainHelper {
+  var useDataProtectionKeychain = false
+  var passwordStore = [String: Data]()
+  let accountName = "OauthTest"
+
+  func password(service: String) throws -> String {
+    guard !service.isEmpty else { throw KeychainWrapper.Error.noService }
+
+    let passwordData = try passwordData(service: service)
+    guard let password = String(data: passwordData, encoding: .utf8) else {
+      throw KeychainWrapper.Error.passwordNotFound
+    }
+    return password
+  }
+
+  func passwordData(service: String) throws -> Data {
+    guard !service.isEmpty else { throw KeychainWrapper.Error.noService }
+
+    guard let passwordData = passwordStore[service + accountName] else {
+      throw KeychainWrapper.Error.passwordNotFound
+    }
+    return passwordData
+  }
+
+  func removePassword(service: String) throws {
+    guard !service.isEmpty else { throw KeychainWrapper.Error.noService }
+
+    guard let _ = passwordStore.removeValue(forKey: service + accountName) else {
+      throw KeychainWrapper.Error.failedToDeletePassword
+    }
+  }
+
+  func setPassword(
+    _ password: String,
+    forService service: String,
+    accessibility: CFTypeRef
+  ) throws {
+    guard let passwordData = password.data(using: .utf8) else {
+      throw KeychainWrapper.Error.unexpectedPasswordData
+    }
+    try setPassword(data: passwordData, forService: service, accessibility: nil)
+  }
+
+  func setPassword(data: Data, forService service: String, accessibility: CFTypeRef?) throws {
+    guard !service.isEmpty else { throw KeychainWrapper.Error.noService }
+    passwordStore.updateValue(data, forKey: service + accountName)
   }
 }
