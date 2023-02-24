693bdcedb5c20ece24fca97c9a0df15baf8cd2ef
Matthew Mathias <mdmathias@google.com>
2023-02-23 17:17:31 -0800

    Use a barrier with the dispatch queue to ensure error can be modified before returned

--- GTMAppAuth/Sources/AuthSession.swift
+++ GTMAppAuth/Sources/AuthSession.swift
@@ -283,9 +283,14 @@ public final class AuthSession: NSObject, GTMSessionFetcherAuthorizer, NSSecureC
     } else {
       args.error = Error.cannotAuthorizeRequest(request as URLRequest)
     }
+    // This can either be a serial queue (since that is what `DispatchQueue.main` is, or it can be
+    // anything, given that we don't know what `fetcherService.callbackQueue` will yield
     let callbackQueue = fetcherService?.callbackQueue ?? DispatchQueue.main
 
-    callbackQueue.async { [weak self] in
+    // Since we want to ensure that `args.error` has a chance to be updated by the `delegate`, we
+    // need to place a barrier here to prevent the below async block of work to complete before the
+    // delegate has a chance to modify the error.
+    callbackQueue.async(flags: [.barrier]) { [weak self] in
       guard let self = self else { return }
 
       if let error = args.error, let delegate = self.delegate {
