449b74ddc97396b8e97fde3101d405d0e64e9dc9
Matthew Mathias <mdmathias@google.com>
2023-04-22 12:36:14 -0700

    Use a semaphore to wait for updated error

--- GTMAppAuth/Sources/AuthSession.swift
+++ GTMAppAuth/Sources/AuthSession.swift
@@ -285,18 +285,29 @@ public final class AuthSession: NSObject, GTMSessionFetcherAuthorizer, NSSecureC
     }
     let callbackQueue = fetcherService?.callbackQueue ?? DispatchQueue.main
 
-    callbackQueue.async(flags: .barrier) {
-      if let error = args.error, let delegate = self.delegate {
-        // Use updated error if exists; otherwise, use whatever is already in `args.error`
-        delegate.updatedError?(forAuthSession: self, originalError: error) { updatedError in
-          args.error = updatedError ?? error
-        }
-      }
-    }
+//    callbackQueue.async(flags: .barrier) {
+//      if let error = args.error, let delegate = self.delegate {
+//        // Use updated error if exists; otherwise, use whatever is already in `args.error`
+//        delegate.updatedError?(forAuthSession: self, originalError: error) { updatedError in
+//          args.error = updatedError ?? error
+//        }
+//      }
+//    }
 
     callbackQueue.async { [weak self] in
       guard let self = self else { return }
 
+      let errorSemaphore = DispatchSemaphore(value: 0)
+      if let error = args.error, let delegate = self.delegate {
+        // Use updated error if exists; otherwise, use whatever is already in `args.error`
+        delegate.updatedError?(forAuthSession: self, originalError: error) { updatedError in
+          args.error = updatedError ?? error
+          errorSemaphore.signal()
+        }
+      } else {
+        errorSemaphore.signal()
+      }
+      _ = errorSemaphore.wait(timeout: .now() + 1)
 
       switch args.callbackStyle {
       case .completion(let callback):
